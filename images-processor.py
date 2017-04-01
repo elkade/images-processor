97249b99f518b981d2e9fa66b0ae79531461823c
elkade <dragan1993@gmail.com>
2017-04-01 21:43:18 +0200

    init

---
+++ images-processor.py
@@ -0,0 +1,128 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+from skimage import data
+from skimage.util.dtype import dtype_range
+from skimage.util import img_as_ubyte
+from skimage import exposure
+from skimage.morphology import disk
+from skimage.filters import rank
+
+import matplotlib
+matplotlib.use('TkAgg')
+
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+import sys
+if sys.version_info[0] < 3:
+    import Tkinter as Tk
+else:
+    import tkinter as Tk
+
+
+def destroy(e):
+    sys.exit()
+
+root = Tk.Tk()
+root.wm_title("Embedding in TK")
+
+
+f = Figure(figsize=(5, 4), dpi=100)
+a = f.add_subplot(111)
+t = arange(0.0, 3.0, 0.01)
+s = sin(2*pi*t)
+
+a.plot(t, s)
+a.set_title('Tk embedding')
+a.set_xlabel('X axis label')
+a.set_ylabel('Y label')
+
+
+# a tk.DrawingArea
+canvas = FigureCanvasTkAgg(f, master=root)
+canvas.show()
+canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+
+canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+
+button = Tk.Button(master=root, text='Quit', command=sys.exit)
+button.pack(side=Tk.BOTTOM)
+
+Tk.mainloop()
+
+matplotlib.rcParams['font.size'] = 9
+
+
+def plot_img_and_hist(img, axes, bins=256):
+    """Plot an image along with its histogram and cumulative histogram.
+
+    """
+    ax_img, ax_hist = axes
+    ax_cdf = ax_hist.twinx()
+
+    # Display image
+    ax_img.imshow(img, cmap=plt.cm.gray)
+    ax_img.set_axis_off()
+
+    # Display histogram
+    ax_hist.hist(img.ravel(), bins=bins)
+    ax_hist.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
+    ax_hist.set_xlabel('Pixel intensity')
+
+    xmin, xmax = dtype_range[img.dtype.type]
+    ax_hist.set_xlim(xmin, xmax)
+
+    # Display cumulative distribution
+    img_cdf, bins = exposure.cumulative_distribution(img, bins)
+    ax_cdf.plot(bins, img_cdf, 'r')
+
+    return ax_img, ax_hist, ax_cdf
+
+
+# Load an example image
+img = img_as_ubyte(data.moon())
+
+# Global equalize
+img_rescale = exposure.equalize_hist(img)
+
+# Equalization
+selem = disk(30)
+img_eq = rank.equalize(img, selem=selem)
+
+
+# Display results
+fig = plt.figure(figsize=(8, 5))
+axes = np.zeros((2, 3), dtype=np.object)
+axes[0, 0] = plt.subplot(2, 3, 1, adjustable='box-forced')
+axes[0, 1] = plt.subplot(2, 3, 2, sharex=axes[0, 0], sharey=axes[0, 0],
+                         adjustable='box-forced')
+axes[0, 2] = plt.subplot(2, 3, 3, sharex=axes[0, 0], sharey=axes[0, 0],
+                         adjustable='box-forced')
+axes[1, 0] = plt.subplot(2, 3, 4)
+axes[1, 1] = plt.subplot(2, 3, 5)
+axes[1, 2] = plt.subplot(2, 3, 6)
+
+ax_img, ax_hist, ax_cdf = plot_img_and_hist(img, axes[:, 0])
+ax_img.set_title('Low contrast image')
+ax_hist.set_ylabel('Number of pixels')
+
+ax_img, ax_hist, ax_cdf = plot_img_and_hist(img_rescale, axes[:, 1])
+ax_img.set_title('Global equalise')
+
+ax_img, ax_hist, ax_cdf = plot_img_and_hist(img_eq, axes[:, 2])
+ax_img.set_title('Local equalize')
+ax_cdf.set_ylabel('Fraction of total intensity')
+
+
+# prevent overlap of y-axis labels
+fig.tight_layout()
+plt.show()
+
+# import skimage
+# import os
+# filename = os.path.join(skimage.data_dir, 'lenna.png')
+# from skimage import io
+# lenna = io.imread(filename)
